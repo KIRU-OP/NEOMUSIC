66de4041766eaeecaf3edf72e3e6025a7ee7febe
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <np564605@gmail.com>
2026-04-26 21:37:01 +0530

    Update API URL for YouTube platform

--- NEOMUSIC/platforms/Youtube.py
+++ NEOMUSIC/platforms/Youtube.py
@@ -30,7 +30,7 @@ class SensitiveDataFilter(logging.Filter):
 logging.getLogger().addFilter(SensitiveDataFilter())
 
 # API URL (Ensure this is working)
-API_URL = "https://shrutibots.site"
+API_URL = "https://kiru-bot.up.railway.app"
 
 # --- UTILS ---
 def get_clean_id(link: str) -> Optional[str]:
