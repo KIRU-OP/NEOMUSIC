5fb87cced7bdbc8a6976803635730857f35339d3
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <np564605@gmail.com>
2026-05-22 18:08:18 +0530

    Add files via upload

--- NEOMUSIC/utils/stream/stream.py
+++ NEOMUSIC/utils/stream/stream.py
@@ -13,7 +13,7 @@ from NEOMUSIC.utils.exceptions import AssistantErr
 from NEOMUSIC.utils.inline import aq_markup, close_markup, stream_markup
 from NEOMUSIC.utils.pastebin import AnonyBin
 from NEOMUSIC.utils.stream.queue import put_queue, put_queue_index
-from NEOMUSIC.utils.thumbnails import gen_thumb
+from NEOMUSIC.utils.thumbnails import get_thumb
 
 
 async def stream(
@@ -31,11 +31,8 @@ async def stream(
 ):
     if not result:
         return
-
     if forceplay:
         await Anony.force_stop_stream(chat_id)
-
-
     if streamtype == "playlist":
         msg = f"{_['play_19']}\n\n"
         count = 0
@@ -52,13 +49,10 @@ async def stream(
                 ) = await YouTube.details(search, False if spotify else True)
             except:
                 continue
-
             if str(duration_min) == "None":
                 continue
-
             if duration_sec > config.DURATION_LIMIT:
                 continue
-
             if await is_active_chat(chat_id):
                 await put_queue(
                     chat_id,
@@ -85,10 +79,6 @@ async def stream(
                     )
                 except:
                     raise AssistantErr(_["play_14"])
-                
-                if not file_path:
-                    raise AssistantErr(_["play_14"])
-
                 await Anony.join_call(
                     chat_id,
                     original_chat_id,
@@ -108,7 +98,7 @@ async def stream(
                     "video" if video else "audio",
                     forceplay=forceplay,
                 )
-                img = await gen_thumb(vidid)
+                img = await get_thumb(vidid)
                 button = stream_markup(_, chat_id)
                 run = await app.send_photo(
                     original_chat_id,
@@ -123,7 +113,6 @@ async def stream(
                 )
                 db[chat_id][0]["mystic"] = run
                 db[chat_id][0]["markup"] = "stream"
-
         if count == 0:
             return
         else:
@@ -141,7 +130,6 @@ async def stream(
                 caption=_["play_21"].format(position, link),
                 reply_markup=upl,
             )
-
     elif streamtype == "youtube":
         link = result["link"]
         vidid = result["vidid"]
@@ -149,22 +137,12 @@ async def stream(
         duration_min = result["duration_min"]
         thumbnail = result["thumb"]
         status = True if video else None
-
-
-        current_queue = db.get(chat_id)
-        if current_queue is not None and len(current_queue) >= 10:
-             return await app.send_message(original_chat_id, "ʏᴏᴜ ᴄᴀɴ'ᴛ ᴀᴅᴅ ᴍᴏʀᴇ ᴛʜᴀɴ 10 sᴏɴɢs ᴛᴏ ᴛʜᴇ ǫᴜᴇᴜᴇ.")
-
         try:
             file_path, direct = await YouTube.download(
                 vidid, mystic, videoid=True, video=status
             )
         except:
             raise AssistantErr(_["play_14"])
-        
-        if not file_path:
-             raise AssistantErr(_["play_14"])
-
         if await is_active_chat(chat_id):
             await put_queue(
                 chat_id,
@@ -206,7 +184,7 @@ async def stream(
                 "video" if video else "audio",
                 forceplay=forceplay,
             )
-            img = await gen_thumb(vidid)
+            img = await get_thumb(vidid)
             button = stream_markup(_, chat_id)
             run = await app.send_photo(
                 original_chat_id,
@@ -221,15 +199,10 @@ async def stream(
             )
             db[chat_id][0]["mystic"] = run
             db[chat_id][0]["markup"] = "stream"
-
     elif streamtype == "soundcloud":
         file_path = result["filepath"]
         title = result["title"]
         duration_min = result["duration_min"]
-        
-        if not file_path:
-            raise AssistantErr(_["play_14"])
-
         if await is_active_chat(chat_id):
             await put_queue(
                 chat_id,
@@ -270,23 +243,18 @@ async def stream(
                 original_chat_id,
                 photo=config.SOUNCLOUD_IMG_URL,
                 caption=_["stream_1"].format(
-                    config.SUPPORT_GROUP, title[:23], duration_min, user_name
+                    config.SUPPORT_CHAT, title[:23], duration_min, user_name
                 ),
                 reply_markup=InlineKeyboardMarkup(button),
             )
             db[chat_id][0]["mystic"] = run
             db[chat_id][0]["markup"] = "tg"
-
     elif streamtype == "telegram":
         file_path = result["path"]
         link = result["link"]
         title = (result["title"]).title()
         duration_min = result["dur"]
         status = True if video else None
-        
-        if not file_path:
-            raise AssistantErr(_["play_5"])
-
         if await is_active_chat(chat_id):
             await put_queue(
                 chat_id,
@@ -333,8 +301,6 @@ async def stream(
             )
             db[chat_id][0]["mystic"] = run
             db[chat_id][0]["markup"] = "tg"
-
-
     elif streamtype == "live":
         link = result["link"]
         vidid = result["vidid"]
@@ -342,7 +308,6 @@ async def stream(
         thumbnail = result["thumb"]
         duration_min = "Live Track"
         status = True if video else None
-        
         if await is_active_chat(chat_id):
             await put_queue(
                 chat_id,
@@ -368,11 +333,6 @@ async def stream(
             n, file_path = await YouTube.video(link)
             if n == 0:
                 raise AssistantErr(_["str_3"])
-            
-
-            if not file_path:
-                raise AssistantErr(_["play_14"])
-
             await Anony.join_call(
                 chat_id,
                 original_chat_id,
@@ -392,7 +352,7 @@ async def stream(
                 "video" if video else "audio",
                 forceplay=forceplay,
             )
-            img = await gen_thumb(vidid)
+            img = await get_thumb(vidid)
             button = stream_markup(_, chat_id)
             run = await app.send_photo(
                 original_chat_id,
@@ -407,17 +367,10 @@ async def stream(
             )
             db[chat_id][0]["mystic"] = run
             db[chat_id][0]["markup"] = "tg"
-
-
     elif streamtype == "index":
         link = result
         title = "ɪɴᴅᴇx ᴏʀ ᴍ3ᴜ8 ʟɪɴᴋ"
         duration_min = "00:00"
-        
-
-        if not link:
-             raise AssistantErr(_["play_14"])
-
         if await is_active_chat(chat_id):
             await put_queue_index(
                 chat_id,
