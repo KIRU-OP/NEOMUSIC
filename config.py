7fd26c043c60ff2522cd7b662ea87b9e4d2e5880
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <np564605@gmail.com>
2026-05-22 13:09:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,8 +24,8 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 BOT_DOCS =  getenv("BOT_DOCS", "https://radha-music.vercel.app")
 MINI_APP =  getenv("MINI_APP", "https://heroku-club.vercel.app")
 
-BASE_API_URL = getenv("BASE_API_URL", "https://youtube-mini.up.railway.app")
-BASE_API_KEY = getenv("BASE_API_KEY", "")
+BASE_API_URL = getenv("BASE_API_URL", "https://t.me/VibeBots")
+BASE_API_KEY = getenv("BASE_API_KEY", "VibeBots_RsszUDPo1HjFWIsPqih")
 
 COOKIES_URL = getenv("COOKIES_URL", None)
 
